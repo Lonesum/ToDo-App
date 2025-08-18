814365821acaa7e9b1162e4948643f3f1c034355
Lonesum <kusharanjanbag@gmailz.com>
2025-08-19 00:03:44 +0530

    Completed

--- src/main/java/com/app/todoapp/controller/TaskController.java
+++ src/main/java/com/app/todoapp/controller/TaskController.java
@@ -3,7 +3,11 @@ package com.app.todoapp.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 import com.app.todoapp.models.Task;
 import com.app.todoapp.services.TaskService;
 import java.util.List;
@@ -22,5 +26,23 @@ public class TaskController {
         List<Task> tasks = taskService.getAllTasks();
         model.addAttribute("tasks",tasks);
         return "tasks";
+    }
+
+    @PostMapping
+    public String createTask(@RequestParam String title){
+        taskService.createTask(title);
+        return "redirect:/";
+    }
+
+    @GetMapping("/{id}/delete")
+    public String deleteTask(@PathVariable Long id){
+        taskService.deleteTask(id);
+        return "redirect:/";
+    }
+
+    @GetMapping("/{id}/toggle")
+    public String toggleTask(@PathVariable Long id){
+        taskService.toggleTask(id);
+        return "redirect:/";
     }
 }
--- src/main/java/com/app/todoapp/services/TaskService.java
+++ src/main/java/com/app/todoapp/services/TaskService.java
@@ -18,5 +18,22 @@ public class TaskService {
         return this.taskRepository.findAll();
     }
       
+     public void createTask(String title) {
+        Task task = new Task();
+        task.setTitle(title);
+        task.setCompleted(false);
+        taskRepository.save(task);
+    }
+
+    public void deleteTask(Long id) {
+        taskRepository.deleteById(id);
+    }
+
+    public void toggleTask(Long id) {
+        Task task = taskRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("Invalid task id"));
+        task.setCompleted(!task.isCompleted());
+        taskRepository.save(task);
+    }
 
 }
